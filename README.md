ebaccc3471bf67b532f13bb088997fecf25fdcb8
WenTzeng <wentzeng@boyu66.net>
2021-05-17 13:13:42 +0800

    chore: add ReadMe

---
+++ README.md
@@ -0,0 +1,5 @@
+docker build -t lottery_demo .
+
+docker run -p 3000:3000 lottery_demo
+
+開啟瀏覽器，前往 localhost:3000/lottery
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,6 +2,9 @@ import { combineReducers, createStore } from 'redux';
 import reducers from '~/stores/reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(combineReducers(reducers), composeWithDevTools());
+const store =
+    process.env.NODE_ENV === 'production'
+        ? createStore(combineReducers(reducers))
+        : createStore(combineReducers(reducers), composeWithDevTools());
 
 export default store;
